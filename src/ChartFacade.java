ddde78b27c6aa3112b6e14165078fbe1f770a5f1
dmcelhaney <143763031+dmcelhaney@users.noreply.github.com>
2024-11-16 17:23:18 -0600

    Implemented the Facade Pattern

---
+++ src/ChartFacade.java
@@ -0,0 +1,35 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ChartFacade {
+    public JFreeChart createBarChart(ArrayList<WCPlayer> players){
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        List<String> distinctYears = players.stream().collect(Collectors.groupingBy(WCPlayer::yearPlayed)).keySet().stream().sorted().map(Object::toString).toList();
+        for(String year : distinctYears) {
+            int numberOfPlayersForYear = players.stream().filter(player -> player.yearPlayed() == Integer.parseInt(year)).collect(Collectors.toList()).size();
+            dataset.addValue(numberOfPlayersForYear, "# of Players", year.substring(2));
+        }
+
+        //Create chart
+        JFreeChart chart = ChartFactory.createBarChart(
+                "Players by Year", // Chart title
+                "Year", // X-axis label
+                "# of Players", // Y-axis label
+                dataset, // Dataset
+                PlotOrientation.VERTICAL, // Orientation
+                true, // Include legend
+                true, // Include tooltips
+                false // Include URLs
+        );
+
+        return chart;
+    }
+}
--- src/PlayerChartPanel.java
+++ src/PlayerChartPanel.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.jfree.chart.*;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 public class PlayerChartPanel extends JPanel {
@@ -14,25 +15,8 @@ public class PlayerChartPanel extends JPanel {
 
     //Creates the bar graph
     public PlayerChartPanel(ArrayList<WCPlayer> players) {
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-
-        List<String> distinctYears = players.stream().collect(Collectors.groupingBy(WCPlayer::yearPlayed)).keySet().stream().sorted().map(Object::toString).toList();
-        for(String year : distinctYears) {
-            int numberOfPlayersForYear = players.stream().filter(player -> player.yearPlayed() == Integer.parseInt(year)).collect(Collectors.toList()).size();
-            dataset.addValue(numberOfPlayersForYear, "# of Players", year.substring(2));
-        }
-
-        //Create chart
-        JFreeChart chart = ChartFactory.createBarChart(
-                "Players by Year", // Chart title
-                "Year", // X-axis label
-                "# of Players", // Y-axis label
-                dataset, // Dataset
-                PlotOrientation.VERTICAL, // Orientation
-                true, // Include legend
-                true, // Include tooltips
-                false // Include URLs
-        );
+        ChartFacade facade = new ChartFacade();
+        JFreeChart chart = facade.createBarChart(players);
 
         ChartPanel chartPanel = new ChartPanel(chart);
         add(chartPanel, BorderLayout.CENTER);
